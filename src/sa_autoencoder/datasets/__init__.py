874c7b8f9213fa7c3c60e9e4cce799531dab98b6
Alexandr Korchemnyi <akorchemnyj@gmail.com>
2022-10-06 21:20:45 +0300

    Dataset mode

--- src/sa_autoencoder/datasets/__init__.py
+++ src/sa_autoencoder/datasets/__init__.py
@@ -2,12 +2,12 @@ from pathlib import Path
 from torch.utils.data import Dataset
 
 from .multi_dsprites import MultiDSprites
-
+from .tetrominoes import Tetrominoes
 
 def get_dataset(path_to_dataset: Path, mode='clevr', validation=False, test=False) -> Dataset:
     assert not (validation & test)
 
-    if mode == 'multi_dsprites':
+    if mode == 'multi_dsprites' or mode == 'tetrominoes':
         if validation:
             split = 'val'
         elif test:
---
+++ src/sa_autoencoder/datasets/tetrominoes.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+
+class Tetrominoes(Dataset):
+    def __init__(self, path_to_dataset: Path):
+        data = np.load(path_to_dataset)
+        self.masks = data['masks']
+        self.images = data['images']
+        self.visibility = data['visibility']
+        self.image_size = self.images[0].shape
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        image = torch.from_numpy(image).float() / 255
+        return image
