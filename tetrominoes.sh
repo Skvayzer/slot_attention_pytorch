2d8c515da1a452b7213c9dcac5395580245d8c3b
Konstantin Smirnov <mrskvayzer@gmail.com>
2022-12-15 02:40:02 +0300

    changed file paths for mine

--- tetrominoes.sh
+++ tetrominoes.sh
@@ -1,4 +1,4 @@
 git pull
 cd ./src/sa_autoencoder/
-python3 train.py --mode "tetrominoes" --path_to_dataset "/home/alexandr_ko/datasets/multi_objects/tetrominoes" --device 0 --batch_size 64 --max_epochs 534 --seed 1
+python3 train.py --mode "tetrominoes" --path_to_dataset "/content/slot_attention_pytorch/datasets/tetrominoes" --path_to_checkpoint "/content/slot_attention_pytorch/src/sa_autoencoder/ckpt/epoch=509-step=477870.ckpt" --device 0 --batch_size 64 --max_epochs 534 --seed 1
 
