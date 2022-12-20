9e26b2a99f715220ee518e3a36357a9505225d2f
Konstantin Smirnov <mrskvayzer@gmail.com>
2022-12-20 07:05:40 +0300

    added clevr launch bash cript

---
+++ clevr.sh
@@ -0,0 +1,3 @@
+git pull
+cd ./src/sa_autoencoder/
+python3 train.py --mode "clevr" --path_to_dataset "/content/slot_attention_pytorch/datasets/clevr" --path_to_checkpoint "/content/slot_attention_pytorch/src/sa_autoencoder/ckpt/epoch=509-step=477870.ckpt" --device 0 --batch_size 64 --max_epochs 534 --seed 1
