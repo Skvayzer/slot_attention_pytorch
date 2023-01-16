f0ffb5ecb1bf85ff88e95cf6a3eea128d32e6a56
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-16 17:58:32 +0300

    prepared clevr

--- datasets/preprocess.sh
+++ datasets/preprocess.sh
@@ -10,8 +10,24 @@
 #SBATCH --no-kill
 #SBATCH --comment="Обучение нейросетевой модели в рамках НИР Центра когнитивного моделирования"
 
-# Load software
-module load anaconda3
 
-# Run python script
-srun python clevr_with_masks.py
+singularity instance start \
+                     --nv  \
+                     --bind /home/AI/yudin.da/smirnov_cv/slot_attention_pytorch/datasets/:/home/ \
+                     /home/AI/yudin.da/smirnov_cv/quantised_sa/ml_env.sif ml_env
+
+singularity exec instance://ml_env /bin/bash -c "
+      source /miniconda/etc/profile.d/conda.sh;
+      conda activate ml_env;
+      export WANDB_API_KEY=c84312b58e94070d15277f8a5d58bb72e57be7fd;
+      set -x;
+      ulimit -Hn;
+      ulimit -Sn;
+      nvidia-smi;
+      free -m;
+      cd /home/smirnov_cv;
+      python3 clevr_with_masks.py;
+      free -m;
+";
+
+singularity instance stop ml_env
