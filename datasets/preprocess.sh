c8ad91963dac53fec223d6195151a7fbdbc915fc
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-01-16 17:50:44 +0300

    prepared clevr

---
+++ datasets/preprocess.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Example of running python script in a batch mode
+
+#SBATCH -J dataset preprocess
+#SBATCH -p normal
+#SBATCH -c 1                            # one CPU core
+#SBATCH -o hello-%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=k.smirnov@innopolis.university
+
+# Load software
+module load anaconda3
+
+# Run python script
+srun python clevr_with_masks.py
